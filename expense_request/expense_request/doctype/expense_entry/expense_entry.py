8f866a1a0e83ca85fac63a72f711804e07a00e41
dandax123 <dandaoluks@gmail.com>
2025-07-17 10:51:54 +0200

    Feat: clean up after cancel

--- expense_request/expense_request/doctype/expense_entry/expense_entry.py
+++ expense_request/expense_request/doctype/expense_entry/expense_entry.py
@@ -7,4 +7,21 @@ from __future__ import unicode_literals
 from frappe.model.document import Document
 
 class ExpenseEntry(Document):
-	pass
+    def on_cancel(self):
+        # Find and cancel linked journal entries
+        je_list = frappe.get_all("Journal Entry", 
+            filters={
+                "bill_no": self.name,
+                "docstatus": 1
+            })
+
+        for je in je_list:
+            journal_entry = frappe.get_doc("Journal Entry", je.name)
+            if journal_entry.docstatus == 1:
+                # Cancel journal entry
+                journal_entry.cancel()
+                frappe.msgprint(f"Journal Entry {journal_entry.name} has been cancelled.")
+
+                # Now delete the journal entry
+                frappe.delete_doc("Journal Entry", journal_entry.name, force=1)
+                frappe.msgprint(f"Journal Entry {journal_entry.name} has been deleted.")
